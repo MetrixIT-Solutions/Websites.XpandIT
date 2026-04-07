40c5afd39ecdb6db8e4ed80310dbfba0bda45939
MetrixIT-Solutions <ashok@metrixit.com>
2026-04-07 16:28:53 +0530

    Xpand IT Solutions - Live Website Code

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Websites.ExpndIT
+# Skill Works IT
+Application: Xpand IT - Website
+
+## Quick start
+
+##########################################
+# Copyright (C) Skill Works IT - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Skill Works IT <contact@skillworksit.com>, Jan 2023
+##########################################
+
+1. Clone this repo using `git clone https://github.com/MetrixIT-Solutions/Websites.XpandIT.git`
+2. Move to the appropriate directory: `cd Websites.XpandIT`.<br />
+
+## License
+
+Skill Works IT license, Copyright (c) 2022 Skill Works IT.
---
+++ assets/js/contactform.js
@@ -0,0 +1,60 @@
+
+/*================================= 
+    Contact Form
+===================================*/
+
+
+
+(function ($) {
+    'use strict';
+    /*
+     * Ajax Contact Form 
+     */
+    $(function () {
+        // Get the form.
+        var form = $('#contact-form');
+        
+        // Get the messages div.
+        var formMessages = $('.form-messege');
+        // Set up an event listener for the contact form.
+        $(form).submit(function (e) {
+            // Stop the browser from submitting the form.
+            e.preventDefault();
+            // Serialize the form data.
+            var formData = $(form).serialize();
+            console.log('================formData:', formData);
+            console.log('================URL:', $(form).attr('action'));
+            // Submit the form using AJAX.
+            $.ajax({
+                    type: 'POST',
+                    url: $(form).attr('action'),
+                    data: formData
+                })
+                .done(function (response) {
+                    // Make sure that the formMessages div has the 'success' class.
+                    $(formMessages).removeClass('error');
+                    $(formMessages).addClass('success');
+
+                    // Set the message text.
+                    $(formMessages).text(response);
+
+                    // Clear the form.
+                    $('#contact-form input,#contact-form textarea').val('');
+                })
+                .fail(function (data) {
+                    // Make sure that the formMessages div has the 'error' class.
+                    $(formMessages).removeClass('success');
+                    $(formMessages).addClass('error');
+
+                    // Set the message text.
+                    if (data.responseText !== '' && data.responseText == 'OK' ) {
+                        $(formMessages).text('Form Submitted Successfully');
+                    } else {
+                        $(formMessages).text('Oops! An error occured and your message could not be sent.');
+                    }
+                });
+        });
+
+    });
+
+})(jQuery);
